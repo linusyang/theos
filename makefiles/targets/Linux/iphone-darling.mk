e9d5a31ec9fde9f91e4c361c500b89f8ef806fef
Linus Yang <laokongzi@gmail.com>
2015-03-18 14:49:38 +0800

    Support Linux native and darling toolchains

--- makefiles/targets/Linux/iphone-darling.mk
+++ makefiles/targets/Linux/iphone-darling.mk
@@ -2,12 +2,12 @@ ifeq ($(_THEOS_TARGET_LOADED),)
 _THEOS_TARGET_LOADED := 1
 THEOS_TARGET_NAME := iphone
 
-SDKTARGET ?= armv7-apple-darwin11
-SDKBINPATH ?= $(THEOS)/toolchain/linux-x64/bin
+SDKTARGET ?= arm-apple-darwin9
+SDKBINPATH ?= $(THEOS)/toolchain/XcodeDefault.xctoolchain/usr/bin
 SYSROOT ?= $(THEOS)/sdk
 ISYSROOT ?= $(SYSROOT)
 
-PREFIX := $(SDKBINPATH)/$(SDKTARGET)-
+PREFIX := dyld $(SDKBINPATH)/
 
 TARGET_CC ?= $(PREFIX)clang
 TARGET_CXX ?= $(PREFIX)clang++
@@ -15,7 +15,7 @@ TARGET_LD ?= $(PREFIX)clang++
 TARGET_STRIP ?= $(PREFIX)strip
 TARGET_STRIP_FLAGS ?= -x
 TARGET_CODESIGN_ALLOCATE ?= $(PREFIX)codesign_allocate
-TARGET_CODESIGN ?= $(PREFIX)ldid
+TARGET_CODESIGN ?= ldid
 TARGET_CODESIGN_FLAGS ?= -S
 
 include $(THEOS_MAKE_PATH)/targets/_common/darwin.mk
